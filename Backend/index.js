f8ce288e5c95851731e437fa1326adb0c8c6688b
Sohel Aktar <154451758+Aktar9775@users.noreply.github.com>
2025-03-19 21:46:57 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -16,6 +16,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true  // Allowed headers
 }));
+app.options('*', cors()); 
 app.use(express.json())
 
 app.use('/url',urlRoute)
